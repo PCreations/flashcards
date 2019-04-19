4cf2000019c8a84c629dd150a24e0f4ba8217614
Pierre Criulanscy <pcriulan@gmail.com>
2019-04-19 16:15:47 +0200

    add missing rue check about the first card to review being the first one in the session deck

--- features/steps/deck.steps.ts
+++ features/steps/deck.steps.ts
@@ -1,11 +1,12 @@
 import flatMap from 'lodash/flatMap';
 import expect from 'expect';
 import { Then } from 'cucumber';
-import { SessionDeckQuery } from '../../src/queries/sessionDeck';
+import { SessionDeckQuery } from '../../src/queries/sessionDeckQuery';
+import { CurrentFlashcardQuestionQuery } from '../../src/queries/currentFlashcardQuestionQuery';
 import { PartitionNumber } from '../../src/domain/box/partitions';
 
 Then(
-  /^the session deck for the box "([\w\W]*)" should contain flashcards from partitions ([1-7]+(?:,[1-7])*)/,
+  /^the session deck for the box "([\w\W]*)" should contain flashcards questions from partitions ([1-7]+(?:,[1-7])*)/,
   async function(boxName, comaSeparatedPartitions) {
     const { boxRepository, authenticationGateway } = this.dependencies;
     const sessionDeckQuery = SessionDeckQuery({ boxRepository });
@@ -17,13 +18,25 @@ Then(
       playerId,
       boxName,
     });
-    const expectedFlashcardsInPartitions = flatMap(
-      partitions,
-      partitionNumber => box.partitions[partitionNumber],
+    const expectedFlashcardsQuestionInPartitions = flatMap(partitions, partitionNumber =>
+      box.partitions[partitionNumber].map(({ question }) => question),
     );
     const theFlashcardsInDeck = await sessionDeckQuery.execute({ boxName, playerId });
-    expect(expectedFlashcardsInPartitions.length).toBeGreaterThan(0);
+    expect(expectedFlashcardsQuestionInPartitions.length).toBeGreaterThan(0);
     expect(theFlashcardsInDeck.length).toBeGreaterThan(0);
-    expect(theFlashcardsInDeck).toEqual(expectedFlashcardsInPartitions);
+    expect(theFlashcardsInDeck).toEqual(expectedFlashcardsQuestionInPartitions);
   },
 );
+
+Then('the first flashcard question to review for the box {string} should be {string}', async function(
+  boxName,
+  flashcardQuestion,
+) {
+  const { boxRepository, authenticationGateway } = this.dependencies;
+  const currentFlashcardQuestionQuery = CurrentFlashcardQuestionQuery({ boxRepository });
+  const currentFlashcardQuestion = await currentFlashcardQuestionQuery.execute({
+    boxName,
+    playerId: authenticationGateway.getCurrentPlayer().id,
+  });
+  expect(currentFlashcardQuestion).toEqual(flashcardQuestion);
+});
