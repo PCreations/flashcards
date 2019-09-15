a280b90e6bb8cd32a965d4cacd288b125fde3411
Pierre Criulanscy <pcriulan@gmail.com>
2019-09-15 17:06:35 +0200

    add a scenario when the user tries to add a flashcard already existing in the box

--- features/addingAFlashcard.feature
+++ features/addingAFlashcard.feature
@@ -28,3 +28,21 @@ Feature: Adding a flashcard in a box
       | England   | London   |
       | Australia | Canberra |
       | France    | Paris    |
+
+  Scenario: Adding a flashcard with the same question than an other flashcard already in the box
+    Given the current player id is player42
+    And the current player has box named Capitals of the World containing flashcards:
+      | partition | question  | answer   |
+      | 1         | England   | London   |
+      | 1         | Australia | Canberra |
+      | 2         | Belgium   | Brussels |
+      | 3         | France    | Paris    |
+      | 4         | Germany   | Berlin   |
+    When the current player wants to add a flashcard in the box Capitals of the World:
+      | question | answer |
+      | France   | Paris  |
+    Then the current player's box Capitals of the World should contain in its first partition the flashcards:
+      | question  | answer   |
+      | England   | London   |
+      | Australia | Canberra |
+    And an error should be returned to inform the player this flashcard is already in the box
--- features/steps/addingAFlashcard.steps.js
+++ features/steps/addingAFlashcard.steps.js
@@ -113,4 +113,69 @@ defineFeature(feature, test => {
       },
     );
   });
+
+  test('Adding a flashcard with the same question than an other flashcard already in the box', ({
+    given,
+    and,
+    when,
+    then,
+  }) => {
+    let currentPlayerId;
+    let addFlashcardInPlayerBoxError;
+    const boxesDb = (() => {
+      const boxes = {};
+      return {
+        get value() {
+          return boxes;
+        },
+      };
+    })();
+    const IO = {
+      getPlayerBoxByName: getPlayerBoxByName.create(boxesDb.value),
+      savePlayerBox: savePlayerBox.create(boxesDb.value),
+    };
+    given(/^the current player id is (.*)$/, playerId => {
+      currentPlayerId = playerId;
+    });
+    and(/the current player has box named (.*) containing flashcards:$/, (boxName, table) => {
+      const partitions = [[], [], [], [], []];
+      table.forEach(
+        ({ partition, question, answer }) =>
+          (partitions[parseInt(partition, 10) - 1] = partitions[parseInt(partition, 10) - 1].concat(
+            createFlashcard({ answer, question }),
+          )),
+      );
+      const box = createBox({ name: boxName, partitions });
+      return IO.savePlayerBox({ playerId: currentPlayerId, box });
+    });
+    when(/^the current player wants to add a flashcard in the box (.*):$/, (boxName, table) => {
+      const theFlashcardToAdd = createFlashcard({
+        question: table[0].question,
+        answer: table[0].answer,
+      });
+      return addFlashcardInPlayerBox({
+        getPlayerBoxByName: IO.getPlayerBoxByName,
+        savePlayerBox: IO.savePlayerBox,
+        playerId: currentPlayerId,
+        boxName,
+        flashcard: theFlashcardToAdd,
+      }).catch(err => {
+        addFlashcardInPlayerBoxError = err;
+      });
+    });
+    then(
+      /^the current player's box (.*) should contain in its first partition the flashcards:$/,
+      async (boxName, table) => {
+        const box = await IO.getPlayerBoxByName({
+          playerId: currentPlayerId,
+          boxName,
+        });
+        const expectedFlashcardsInFirstPartition = table.map(createFlashcard);
+        expect(box.partitions[0]).toEqual(expectedFlashcardsInFirstPartition);
+      },
+    );
+    and('an error should be returned to inform the player this flashcard is already in the box', () => {
+      expect(addFlashcardInPlayerBoxError).toMatch('Flashcard already in the box');
+    });
+  });
 });
