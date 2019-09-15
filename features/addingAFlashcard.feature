f610fb4ce9621bc1171ccd8329eceb7821a06d31
Pierre Criulanscy <pcriulan@gmail.com>
2019-09-15 16:38:36 +0200

    add second scenario to force us to implement a new business rule

--- features/addingAFlashcard.feature
+++ features/addingAFlashcard.feature
@@ -10,6 +10,21 @@ Feature: Adding a flashcard in a box
     When the current player wants to add a flashcard in the box Capitals of the World:
       | question | answer |
       | France   | Paris  |
-    Then the current player's box Capitals of the World should contain in its first partition the flashcard:
+    Then the current player's box Capitals of the World should contain in its first partition the flashcards:
       | question | answer |
       | France   | Paris  |
+
+  Scenario: Adding a flashcard in an already existing box with a not empty first partition
+    Given the current player id is player42
+    And the current player has box named Capitals of the World containing flashcards:
+      | partition | question  | answer   |
+      | 1         | England   | London   |
+      | 1         | Australia | Canberra |
+    When the current player wants to add a flashcard in the box Capitals of the World:
+      | question | answer |
+      | France   | Paris  |
+    Then the current player's box Capitals of the World should contain in its first partition the flashcards:
+      | question  | answer   |
+      | England   | London   |
+      | Australia | Canberra |
+      | France    | Paris    |
--- features/steps/addingAFlashcard.steps.js
+++ features/steps/addingAFlashcard.steps.js
@@ -1,5 +1,6 @@
 const { defineFeature, loadFeature } = require('jest-cucumber');
 const { createFlashcard } = require('../../src/createFlashcard');
+const { createBox, addFlashcard } = require('../../src/box');
 const getPlayerBoxByName = require('../../src/box/io/getPlayerBoxByName');
 const savePlayerBox = require('../../src/box/io/savePlayerBox');
 const { addFlashcardInPlayerBox } = require('../../src/useCases/addFlashcardInPlayerBox');
@@ -7,13 +8,13 @@ const { addFlashcardInPlayerBox } = require('../../src/useCases/addFlashcardInPl
 const feature = loadFeature('./features/addingAFlashcard.feature');
 
 defineFeature(feature, test => {
-  let currentPlayerId;
-  const boxesDb = {};
-  const IO = {
-    getPlayerBoxByName: getPlayerBoxByName.create(boxesDb),
-    savePlayerBox: savePlayerBox.create(boxesDb),
-  };
   test('Adding a flashcard in a new box', ({ given, and, when, then }) => {
+    let currentPlayerId;
+    const boxesDb = {};
+    const IO = {
+      getPlayerBoxByName: getPlayerBoxByName.create(boxesDb),
+      savePlayerBox: savePlayerBox.create(boxesDb),
+    };
     given(/^the current player id is (.*)$/, playerId => {
       currentPlayerId = playerId;
     });
@@ -38,7 +39,7 @@ defineFeature(feature, test => {
     });
 
     then(
-      /^the current player's box (.*) should contain in its first partition the flashcard:$/,
+      /^the current player's box (.*) should contain in its first partition the flashcards:$/,
       async (boxName, table) => {
         const box = await IO.getPlayerBoxByName({
           playerId: currentPlayerId,
@@ -50,6 +51,65 @@ defineFeature(feature, test => {
         });
 
         expect(box.partitions[0]).toContainEqual(theExpectedFlashcard);
+      },
+    );
+  });
+
+  test('Adding a flashcard in an already existing box with a not empty first partition', ({
+    given,
+    and,
+    when,
+    then,
+  }) => {
+    let currentPlayerId;
+    const boxesDb = (() => {
+      const boxes = {};
+      return {
+        get value() {
+          return boxes;
+        },
+      };
+    })();
+    const IO = {
+      getPlayerBoxByName: getPlayerBoxByName.create(boxesDb.value),
+      savePlayerBox: savePlayerBox.create(boxesDb.value),
+    };
+    given(/^the current player id is (.*)$/, playerId => {
+      currentPlayerId = playerId;
+    });
+    and(/the current player has box named (.*) containing flashcards:$/, (boxName, table) => {
+      const partitions = [[], [], [], [], []];
+      table.forEach(
+        ({ partition, question, answer }) =>
+          (partitions[parseInt(partition, 10) - 1] = partitions[parseInt(partition, 10) - 1].concat(
+            createFlashcard({ answer, question }),
+          )),
+      );
+      const box = createBox({ name: boxName, partitions });
+      return IO.savePlayerBox({ playerId: currentPlayerId, box });
+    });
+    when(/^the current player wants to add a flashcard in the box (.*):$/, (boxName, table) => {
+      const theFlashcardToAdd = createFlashcard({
+        question: table[0].question,
+        answer: table[0].answer,
+      });
+      return addFlashcardInPlayerBox({
+        getPlayerBoxByName: IO.getPlayerBoxByName,
+        savePlayerBox: IO.savePlayerBox,
+        playerId: currentPlayerId,
+        boxName,
+        flashcard: theFlashcardToAdd,
+      });
+    });
+    then(
+      /^the current player's box (.*) should contain in its first partition the flashcards:$/,
+      async (boxName, table) => {
+        const box = await IO.getPlayerBoxByName({
+          playerId: currentPlayerId,
+          boxName,
+        });
+        const expectedFlashcardsInFirstPartition = table.map(createFlashcard);
+        expect(box.partitions[0]).toEqual(expectedFlashcardsInFirstPartition);
       },
     );
   });
